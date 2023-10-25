634dbd8c24af9c420942995317e2b1a4eca09be2
ming <132370137+yigecom@users.noreply.github.com>
2023-10-26 01:08:46 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,16 +3,14 @@ export const APP_ID = 'e9740760-623a-4a90-ae6d-7ead171f0e9c'
 export const API_KEY = 'app-6WXCEmZIfP65XQmYuQ1qdQo9'
 export const API_URL = 'https://api.dify.ai/v1'
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP''
+  title: 'Chat APP',
   description: '',
-  copyright: 'migu',
-  privacy_policy: '
+  copyright: '',
+  privacy_policy: '',
   default_language: 'zh-Hans',
 }
 
 export const isShowPrompt = false
-
-
 export const promptTemplate = 'I want you to act as a javascript console.'
 
 export const API_PREFIX = '/api'
@@ -20,3 +18,4 @@ export const API_PREFIX = '/api'
 export const LOCALE_COOKIE_NAME = 'locale'
 
 export const DEFAULT_VALUE_MAX_LEN = 48
+
