9e373080e189325aa40a8247dbb3a780269e7ee9
ming <132370137+yigecom@users.noreply.github.com>
2023-10-25 23:02:15 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,11 +1,11 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
-export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
-export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
+export const APP_ID = `e9740760-623a-4a90-ae6d-7ead171f0e9c`
+export const API_KEY = `app-ITXwry2OswPH2TovRFkKyVup`
+export const API_URL = `https://api.dify.ai/v1`
 export const APP_INFO: AppInfo = {
   title: 'Chat APP',
   description: '',
-  copyright: '',
+  copyright: 'migu',
   privacy_policy: '',
   default_language: 'zh-Hans',
 }
