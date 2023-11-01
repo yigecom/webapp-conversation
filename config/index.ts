9a8aa6112344af7037630e5e827a46135e557521
migu <132370137+yigecom@users.noreply.github.com>
2023-11-01 17:34:41 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 import type { AppInfo } from '@/types/app'
 export const APP_ID = 'e9740760-623a-4a90-ae6d-7ead171f0e9c'
 export const API_KEY = 'app-6WXCEmZIfP65XQmYuQ1qdQo9'
-export const API_URL = 'https://vercel-dns.com'
+export const API_URL = 'https://api.dify.ai/v1'
 export const APP_INFO: AppInfo = {
   title: 'migu APP',
   description: '',
